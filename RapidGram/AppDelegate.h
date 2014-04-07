ecffc964d5de769eebf0a9b7e9a50cdae31b8fe4
Steve Toosevich <stoosevich@gmail.com>
2014-04-07 13:53:42 -0500

    Initial Commit

---
+++ RapidGram/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RapidGram
+//
+//  Created by Steve Toosevich on 4/7/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RapidGramTests/RapidGramTests.m
@@ -0,0 +1,34 @@
+//
+//  RapidGramTests.m
+//  RapidGramTests
+//
+//  Created by Steve Toosevich on 4/7/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RapidGramTests : XCTestCase
+
+@end
+
+@implementation RapidGramTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
